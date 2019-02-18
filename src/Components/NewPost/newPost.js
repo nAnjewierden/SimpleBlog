b7c7118212feba0a67146cb2ed2c882c6899a18f
Nicholas Anjewierden <nicholasanjewierden0@gmail.com>
2019-02-18 13:57:23 -0700

    New post formating, added button

--- src/Components/NewPost/newPost.js
+++ src/Components/NewPost/newPost.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import { Form, FormGroup, Label, Input } from 'reactstrap';
+import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
 import axios from 'axios'
 import './reset.css'
 import './NewPost.css'
@@ -18,13 +18,14 @@ export default class NewPost extends Component{
     <div className='mainDiv'>
         <h1 className='header'>New Post</h1>
         <Form>
-            <FormGroup>
+            <FormGroup row>
                 <Label for="exampleEmail">Title</Label>
-                <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+                <Input type="email" name="email" id="postTitle" placeholder="Title of post" />
             </FormGroup>
-            <FormGroup>
+            <FormGroup row>
                 <Label for="exampleText">Content</Label>
-                <Input type="textarea" name="text" id="exampleText" />
+                <Input type="textarea" name="text" id="postContent" />
+                <Button color='primary' outline>Post</Button>
             </FormGroup>
         </Form>
     </div>
